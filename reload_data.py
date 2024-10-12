d3f1f2f2a61cd2cece5082ed6172a722ea0b1eb0
lfletcher23 <84860963+lfletcher23@users.noreply.github.com>
2024-10-11 23:19:55 -0400

    fixed reqs_ref example

--- reload_data.py
+++ reload_data.py
@@ -1,7 +1,9 @@
 from Setup import admin_functions as ad
+import basic_data_funcs as bd
 
 # reload all files
 #ad.reload_all("test_reload", save_all_files=True)
 
 # reload just buckets and programs
-#ad.reload_buckets("name_prefix", save_all_files=True)
+#reqs_ref = bd.get_dict_from_json("test_reload_reqs.json")
+#ad.reload_buckets("test_reload2", reqs_ref, save_all_files=True)
